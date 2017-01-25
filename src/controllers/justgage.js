457b0e55c5ed0d6869de60addcc0e9035327c08a
Daniel Raban <github@danielraban.com>
2017-01-25 11:07:28 -0500

    Update justgage.js

--- src/controllers/justgage.js
+++ src/controllers/justgage.js
@@ -1,5 +1,4 @@
 'use strict';
- justgageCtrl.$inject = ['$scope', '$filter'];
 angular.module('frapontillo.gage.controllers', [])
   .controller('justgageCtrl', function ($scope, $filter) {
     var self = this;
